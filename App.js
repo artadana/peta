3d5e4dce20fc44b7b39cece1b197f36871a787f1
artadana <33562250+artadana@users.noreply.github.com>
2018-05-25 00:11:04 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
+
+  state = {
+    region: {
+      latitude: -8.149407,
+      longitude: 115.216667,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.374209,
+          longitude: 115.451023
+        },
+        title: 'Pura Besakih',
+        subtitle: 'Besakih, Rendang, Kabupaten Karangasem, Bali 80863'
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.551324,
+          longitude:115.469176
+        },
+        title: 'Pura Goa Lawah',
+        subtitle: 'Pura Goa Lawah, Jalan Raya Goa Lawah, Pesinggahan, Kabupaten Klungkung, Bali'
+      },
+
+   ]
+  };
+
+  render() {
+    return (
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              Peta Pura
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}> @I Made Artadana </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
